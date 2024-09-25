50e49d267d3bbe8d2d39d2798cfcbebd60c2006a
vanillawhey <cedric.derstroff@gmx.net>
2024-09-25 11:10:53 +0200

    Add Vit; Update OCT

--- cleanrl/ppo_atari_vit.py
+++ cleanrl/ppo_atari_vit.py
@@ -10,6 +10,7 @@ import numpy as np
 
 from tqdm import tqdm
 from rtpt import RTPT
+from pathlib import Path
 from dataclasses import dataclass
 
 import gymnasium as gym
@@ -37,8 +38,9 @@ if oc_atari_dir is not None:
     sys.path.insert(1, a)
 
 from ocatari.core import OCAtari
-from ocrltransformer.wrappers import OCWrapper
-from torch.nn import TransformerEncoderLayer, TransformerEncoder
+
+from vit_pytorch import SimpleViT
+
 
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -60,7 +62,7 @@ class Args:
     # Environment
     env_id: str = "ALE/Pong-v5"
     """the id of the environment"""
-    obs_mode: str = "obj"
+    obs_mode: str = "dqn"
     """observation mode for OCAtari"""
     feature_func: str = "xywh"
     """the object features to use as observations"""
@@ -94,7 +96,7 @@ class Args:
     """total timesteps of the experiments"""
     learning_rate: float = 2.5e-4
     """the learning rate of the optimizer"""
-    num_envs: int = 10
+    num_envs: int = 1
     """the number of parallel game environments"""
     num_steps: int = 128
     """the number of steps to run in each environment per policy rollout"""
@@ -124,12 +126,14 @@ class Args:
     """the target KL divergence threshold"""
 
     # Transformer
-    emb_dim: int = 64
+    emb_dim: int = 128
     """input embedding size of the transformer"""
     num_heads: int = 8
     """number of multi-attention heads"""
-    num_blocks: int = 2
+    num_blocks: int = 4
     """number of transformer blocks"""
+    patch_size: int = 12
+    """ViT patch size"""
 
     # to be filled in runtime
     batch_size: int = 0
@@ -158,7 +162,7 @@ def make_env(env_id, idx, capture_video, run_dir, feature_func, window_size):
                           render_mode="rgb_array", render_oc_overlay=False)
         elif args.backend == 0:
             logger.info("Using OCAtari backend")
-            env = OCAtari(env_id, mode="ram", hud=False, render_mode="rgb_array",
+            env = OCAtari(env_id, hud=False, render_mode="rgb_array",
                           render_oc_overlay=False, obs_mode=args.obs_mode,
                           logger=logger, feature_func=feature_func,
                           buffer_window_size=window_size)
@@ -179,7 +183,6 @@ def make_env(env_id, idx, capture_video, run_dir, feature_func, window_size):
         # env = gym.wrappers.ResizeObservation(env, (84, 84))
         # env = gym.wrappers.GrayScaleObservation(env)
         # env = gym.wrappers.FrameStack(env, 4)
-        env = OCWrapper(env)
 
         return env
 
@@ -193,21 +196,25 @@ def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
 
 
 class PPOAgent(nn.Module):
-    def __init__(self, envs, emb_dim, num_heads, num_blocks, device):
+    def __init__(self, envs, emb_dim, num_heads, num_blocks, patch_size,
+                 buffer_window_size, device):
         super().__init__()
-        dims = envs.observation_space.feature_space.shape
-
-        encoder_layer = TransformerEncoderLayer(emb_dim, num_heads,
-                                                emb_dim, device=device,
-                                                dropout=0.1, batch_first=True)
 
         self.network = nn.Sequential(
-            nn.Linear(dims[1], emb_dim, device=device),
-            TransformerEncoder(encoder_layer, num_blocks),
+            SimpleViT(
+                image_size=84,
+                patch_size=patch_size,
+                channels=buffer_window_size,
+                num_classes=emb_dim,
+                dim=emb_dim,
+                depth=num_blocks,
+                heads=num_heads,
+                mlp_dim=emb_dim,
+            ),
             nn.Flatten(),
         )
-        self.actor = layer_init(nn.Linear(dims[0] * emb_dim, envs.action_space.n, device=device), std=0.01)
-        self.critic = layer_init(nn.Linear(dims[0] * emb_dim, 1, device=device), std=1)
+        self.actor = layer_init(nn.Linear(emb_dim, envs.action_space.n, device=device), std=0.01)
+        self.critic = layer_init(nn.Linear(emb_dim, 1, device=device), std=1)
 
     def get_value(self, x):
         return self.critic(self.network(x))
@@ -279,11 +286,12 @@ if __name__ == "__main__":
     envs.seed(args.seed)
     # assert isinstance(envs.single_action_space, gym.spaces.Discrete), "only discrete action space is supported"
 
-    agent = PPOAgent(envs, args.emb_dim, args.num_heads, args.num_blocks, device)
+    agent = PPOAgent(envs, args.emb_dim, args.num_heads, args.num_blocks,
+                     args.patch_size, device)
     optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
 
     # ALGO Logic: Storage setup
-    obs = torch.zeros((args.num_steps, args.num_envs) + envs.observation_space.feature_space.shape).to(device)
+    obs = torch.zeros((args.num_steps, args.num_envs) + envs.observation_space.shape).to(device)
     actions = torch.zeros((args.num_steps, args.num_envs) + envs.action_space.shape).to(device)
     logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
     rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
@@ -361,7 +369,7 @@ if __name__ == "__main__":
             returns = advantages + values
 
         # flatten the batch
-        b_obs = obs.reshape((-1,) + envs.observation_space.feature_space.shape)
+        b_obs = obs.reshape((-1,) + envs.observation_space.shape)
         b_logprobs = logprobs.reshape(-1)
         b_actions = actions.reshape((-1,) + envs.action_space.shape)
         b_advantages = advantages.reshape(-1)
