8ddef429cbd135cbcea471ab4266072afb7c2c0d
vanillawhey <cedric.derstroff@gmx.net>
2026-03-12 10:45:50 +0100

    Fix: Rainbow loading

--- cleanrl/architectures/loading.py
+++ cleanrl/architectures/loading.py
@@ -2,6 +2,54 @@ import torch
 from torch import nn
 
 from .common import NormalizeImg
+from .rainbow import NoisyLinear, F
+
+
+class NoisyDuelingDistributionalNetwork(nn.Module):
+    def __init__(self, env, n_atoms, v_min, v_max, n_actions):
+        super().__init__()
+        self.n_atoms = n_atoms
+        self.v_min = v_min
+        self.v_max = v_max
+        self.delta_z = (v_max - v_min) / (n_atoms - 1)
+        self.n_actions = n_actions
+        self.register_buffer("support", torch.linspace(v_min, v_max, n_atoms))
+
+        self.network = nn.Sequential(
+            nn.Conv2d(env.observation_space.shape[0], 32, 8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, stride=1),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+        conv_output_size = 3136
+
+        self.value_head = nn.Sequential(NoisyLinear(
+            conv_output_size, 512), nn.ReLU(), NoisyLinear(512, n_atoms))
+
+        self.advantage_head = nn.Sequential(
+            NoisyLinear(conv_output_size, 512), nn.ReLU(
+            ), NoisyLinear(512, n_atoms * self.n_actions)
+        )
+
+    def forward(self, x):
+        h = self.network(x / 255.0)
+        value = self.value_head(h).view(-1, 1, self.n_atoms)
+        advantage = self.advantage_head(
+            h).view(-1, self.n_actions, self.n_atoms)
+        q_atoms = value + advantage - advantage.mean(dim=1, keepdim=True)
+        q_dist = F.softmax(q_atoms, dim=2)
+        return q_dist
+
+
+    def draw_action(self, obs):
+        q_dist = self.forward(obs)
+        q_values = torch.sum(q_dist * self.support, dim=2)
+        actions = torch.argmax(q_values, dim=1)
+        return actions
+
 
 class PPOAgentFeatures(nn.Module):
     def __init__(self, env, device, normalize=True):
@@ -109,7 +157,20 @@ class PPODefault(nn.Module):
 
 
 def init_agent(env, ckpt, device):
-    if "network.0.weight" in ckpt["model_weights"]:
+    if "advantage_head.2.weight_mu" in ckpt["model_weights"]:
+        args = ckpt["args"]
+        agent = NoisyDuelingDistributionalNetwork(
+            env,
+            args["n_atoms"],
+            args["v_min"],
+            args["v_max"],
+            env.action_space.n
+        )
+        agent.load_state_dict(ckpt["model_weights"])
+        agent.eval()
+        return agent
+
+    elif "network.0.weight" in ckpt["model_weights"]:
         agent_class = PPOAgentNetwork
     elif "network.1.weight" in ckpt["model_weights"]:
         agent_class = PPODefault
@@ -120,4 +181,5 @@ def init_agent(env, ckpt, device):
 
     agent = agent_class(env, device)
     agent.load_state_dict(ckpt["model_weights"])
+    agent.eval()
     return agent
